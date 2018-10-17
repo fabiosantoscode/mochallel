93fae411d3a5e4cf9fc8fcc6592d00641e7777a7
Fábio Santos <fabiosantosart@gmail.com>
2018-10-17 21:24:00 +0100

    change test timeout

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var path = require('path')
 
 describe('mochallel', function () {
   it('can spawn parallel mocha processes', function (done) {
-    this.timeout(10 * 1000)
+    this.timeout(4 * 1000)
     var mocha = new Mochallel({ maxParallel: 1 })
     mocha.addFile(path.join(__dirname, 'tests/example'))
     mocha.run(function (code) {

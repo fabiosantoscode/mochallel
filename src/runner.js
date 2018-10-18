3d6dc43129732d696089da2febd02c3a54ef0ed3
Fábio Santos <fabiosantosart@gmail.com>
2018-10-18 16:53:01 +0100

    make mocha global

--- src/runner.js
+++ src/runner.js
@@ -5,6 +5,7 @@ const Mocha = require('mocha')
 process.on('message', ({ type, file, options }) => {
   if (type === 'test') {
     const mocha = new Mocha(options)
+    global.mocha = mocha
     mocha.addFile(file)
     mocha.run((code) => {
       process.send(JSON.stringify({ code }))

c6a2669d513ef43d0fd75e9d922d2db9a3f24c6a
Fábio Santos <fabiosantosart@gmail.com>
2018-11-02 19:57:07 +0000

    test the CLI

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert')
 var path = require('path')
+var child_process = require('child_process')
 var Mochallel = require('..')
 
 describe('mochallel', function () {
@@ -18,6 +19,13 @@ describe('mochallel', function () {
     mocha.addFile(path.join(__dirname, 'tests/failing-test'))
     mocha.run(function (code) {
       assert.strictEqual(code, 1)
+      done()
+    })
+  })
+  it('CLI works', function (done) {
+    child_process.exec('bin/mochallel test/tests/example', function (err, stdout, stderr) {
+      if (err) throw err
+      assert(/2 passing/.test(stdout))
       done()
     })
   })

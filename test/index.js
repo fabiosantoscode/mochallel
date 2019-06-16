b2e40e99c6516d363c6a19e716e25456c4d7efa5
Fábio Santos <fabiosantosart@gmail.com>
2019-06-16 15:55:23 +0100

    move failing test up the chain

--- test/index.js
+++ test/index.js
@@ -7,18 +7,18 @@ var Mochallel = require('..')
 
 describe('mochallel', function () {
   this.timeout(16 * 1000)
-  it('can spawn parallel mocha processes', function (done) {
-    var mocha = new Mochallel({ maxParallel: 1 })
-    mocha.addFile(path.join(__dirname, 'tests/example'))
-    mocha.run(function (code) {
-      done()
-    })
-  })
   it('can run failing tests', function (done) {
     var mocha = new Mochallel({ maxParallel: 1 })
     mocha.addFile(path.join(__dirname, 'tests/failing-test'))
     mocha.run(function (code) {
       assert.strictEqual(code, 1)
+      done()
+    })
+  })
+  it('can spawn parallel mocha processes', function (done) {
+    var mocha = new Mochallel({ maxParallel: 1 })
+    mocha.addFile(path.join(__dirname, 'tests/example'))
+    mocha.run(function (code) {
       done()
     })
   })
